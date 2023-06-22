e5a5f49651a8b307e6844cefdb7abe406b3a3ba6
Sarah Brooks <sarahebrooks12@gmail.com>
2023-06-21 22:38:38 -0400

    finished sort of

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+        Owner AddOwner(Owner owner);
+        Owner UpdateOwner(Owner owner);
+        Owner DeleteOwner(int id);
+        Owner GetOwnerByEmail(string email);
+    }
+}
