6a9a69ad22e96aa3d16846812d388d39384efaf3
Sarah Brooks <sarahebrooks12@gmail.com>
2023-06-23 15:01:47 -0400

    chapter 3

---
+++ SQLQuery2.sql
@@ -0,0 +1,5 @@
+﻿  SELECT Owner.Id, Owner.Email, Owner.[Name], Owner.Address, Owner.NeighborhoodId, Neighborhood.Name as NeighborhoodName, Owner.Phone, Dog.Id as DogId, Dog.Name as DogName, Dog.Breed, Dog.Notes, Dog.ImageUrl
+                        FROM Owner
+                        LEFT JOIN Neighborhood on Neighborhood.Id = Owner.NeighborhoodId
+                        LEFT JOIN Dog on Dog.OwnerId = Owner.Id
+                        Where Owner.Id = 1
